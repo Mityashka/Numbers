ff3839977493c7251e65b6044a0d0a7db8db2dcd
Дмитрий Кузькин <dimakuzkin119@gmail.com>
2023-03-23 22:04:45 +0300

    Добавьте файлы проекта.

---
+++ Fingers/Fingers.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "RU");
+	int a;
+	char exit;
+	for (;;) {
+		cout << "введите число: ";
+		cin >> a;
+		cout << "Квадрат числа> " << a << " < равен: " << a * a << endl;
+		cout << "если вы хотите продолжить ввод, напишите yes, если хотите прервать цикл, напишите no: ";
+		cin >> exit;
+		if (exit != 'yes' && exit != 'Yes' && exit != 'Y' && exit != 'y')
+			break;
+	}
+
+	return 0;
+}
